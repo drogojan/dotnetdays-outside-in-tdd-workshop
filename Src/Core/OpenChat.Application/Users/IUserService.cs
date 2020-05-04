8dc76c51b17aa99d4b7bc728ac896a5496e32d96
Balan Catalin <catalin.balan@softvision.ro>
2020-05-04 16:53:29 +0300

    Second-session: unit test for users controller post method.

---
+++ Src/Core/OpenChat.Application/Users/IUserService.cs
@@ -0,0 +1,7 @@
+namespace OpenChat.Application.Users
+{
+    public interface IUserService
+    {
+        void AddUser(UserRequest userRequest);
+    }
+}
--- Tests/OpenChat.API.UnitTests/UsersControllerTests.cs
+++ Tests/OpenChat.API.UnitTests/UsersControllerTests.cs
@@ -1,7 +1,41 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using OpenChat.API.Controllers;
+using OpenChat.Application.Users;
+using Xunit;
+
 namespace OpenChat.API.UnitTests
 {
     public class UsersControllerTests
     {
-        
+        [Fact]
+        public async Task Post_Should_Return_The_Newly_Created_User()
+        {
+            var userRequest = new UserRequest();
+            var expectedResponse = new UserResponse();
+            var userServiceMock = new Mock<IUserService>();
+            var userController = new UsersController(userServiceMock.Object);
+
+            var response = await userController.Post(userRequest) as CreatedResult;
+            var userResponse = response?.Value as UserResponse;
+
+            response?.Value.Should().BeOfType<UserResponse>();
+            userResponse.Should().BeEquivalentTo(expectedResponse);
+        }
+
+        [Fact]
+        public async Task Post_Should_Invoke_UserService()
+        {
+            var userServiceMock = new Mock<IUserService>();
+            
+            var userRequest = new UserRequest();
+            var userController = new UsersController(userServiceMock.Object);
+
+            await userController.Post(userRequest);
+
+            userServiceMock.Verify(us => us.AddUser(userRequest));
+        }
     }
 }
